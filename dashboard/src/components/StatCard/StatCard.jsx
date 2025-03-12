36d8bd9239baadc5552a2643b53271f9b7510acc
ChamithEC <chamithwijesooriya@gmail.com>
2025-03-12 08:01:34 +0530

    Stat Card updated

--- dashboard/src/components/StatCard/StatCard.jsx
+++ dashboard/src/components/StatCard/StatCard.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './StatCard.css'
+import { Grid, GridItem } from "@chakra-ui/react"
 
 const StatCard = () => {
   return (
     <div className='stat-card-container'>
-        <div className='stat-card-content'>
-            <h1>Stat Card</h1>
-        </div>
+      <Grid templateColumns="repeat(3, 1fr)" gap="6">
+        <GridItem>
+          Count 1
+        </GridItem>
+        <GridItem>
+          Count 2
+        </GridItem>
+        <GridItem>
+          Count 3
+        </GridItem>
+      </Grid>
     </div>
   )
 }
