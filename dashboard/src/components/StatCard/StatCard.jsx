464a88b913943c9a1332c558c50c1041fde0109d
ChamithEC <chamithwijesooriya@gmail.com>
2025-03-14 00:06:00 +0530

    Stat cards UI done

--- dashboard/src/components/StatCard/StatCard.jsx
+++ dashboard/src/components/StatCard/StatCard.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import './StatCard.css'
-import { Grid, GridItem } from "@chakra-ui/react"
+import React from "react";
+import "./StatCard.css";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const StatCard = () => {
+  const counts = {
+    dailyActiveUsers: {
+      count: 1051,
+      date: "18 Mar 2020",
+    },
+    monthlyActiveUsers: {
+      count: 60032,
+      date: "18 Mar 2020",
+    },
+    dailyPerActiveUser: {
+      count: 23,
+      date: "18 Mar 2020",
+    },
+  }
   return (
-    <div className='stat-card-container'>
+    <div className="stat-card-container">
       <Grid templateColumns="repeat(3, 1fr)" gap="6">
         <GridItem>
-          Count 1
+          <div className="stat-card-count-title">Daily Active Users</div>
+          <div className="stat-card-count">{counts.dailyActiveUsers.count}</div>
+          <div className="stat-card-date">{counts.dailyActiveUsers.date}</div>
         </GridItem>
         <GridItem>
-          Count 2
+          <div className="stat-card-count-title">Monthly Active Users</div>
+          <div className="stat-card-count">{counts.monthlyActiveUsers.count}</div>
+          <div className="stat-card-date">{counts.monthlyActiveUsers.date}</div>
         </GridItem>
         <GridItem>
-          Count 3
+          <div className="stat-card-count-title">Daily Time Per Active User</div>
+          <div className="stat-card-count">{counts.dailyPerActiveUser.count}</div>
+          <div className="stat-card-date">{counts.dailyPerActiveUser.date}</div>
         </GridItem>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default StatCard
+export default StatCard;
